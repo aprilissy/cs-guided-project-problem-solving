6a75fe813749b0a9797fe7b7b8bb836d22025b0d
April Darger <april.m.darger@gmail.com>
2021-01-12 21:21:03 -0700

    Self #6.

--- src/demonstration_06.py
+++ src/demonstration_06.py
@@ -8,5 +8,12 @@ We will consider `a, e, i, o, u as vowels for this challenge (but not y).
 The input string will only consist of lower case letters and/or spaces.
 """
 def get_count(input_str):
-    # Your code here
+    print(*map(input_str.lower().count, 'aeiou'))
+    vowels = 0
+    for each in input_str:
+        if each in 'aeiouAEIOU':
+            vowels += 1
+    return vowels
 
+print(get_count('hello My name is April')) # 7
+print(get_count('jklsdjfoewijrfsdkjfioweujrijsdflsufoiaje;a')) # 7
